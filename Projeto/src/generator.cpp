71cbe15acbdcdba207afc54a6c09de4a02f34a88
Diogoesteves11 <diogojfe@outlook.com>
2025-02-10 21:21:09 +0000

    gitignore added

--- Projeto/src/generator.cpp
+++ Projeto/src/generator.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 #include <cmath>
+
+
+#define SPHERE "sphere"
+#define PLANE "plane"
+#define BOX "box"
+#define CONE "cone"
 
 void geraEsfera(float raio, int slices, int stacks, char* outputFile)
 {
