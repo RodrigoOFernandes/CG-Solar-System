e37e7437d1391a143b10c1c7660520ac1f51d30e
Diogoesteves11 <diogojfe@outlook.com>
2025-03-06 19:55:24 +0000

    cenas

--- Projeto/engine/include/config/group.hpp
+++ Projeto/engine/include/config/group.hpp
@@ -2,7 +2,20 @@
 #define GROUP_HPP
 
 
+#include "../include/parser/tinyxml2.h"
+#include "../include/config/modelfile.hpp"
+#include "../include/config/transforms.hpp"
 
+#include <iostream>
+#include <vector>
+
+
+
+class Group{
+    public: 
+        ModelFile file;
+        std::vector<Transforms> transforms;
+};
 
 
 #endif
