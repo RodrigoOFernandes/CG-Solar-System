33c013208f75d5f965e46000565952e87a6d6f0d
RodrigoOFernandes <rodrigofernan132004@gmail.com>
2025-03-07 09:14:01 +0000

    inicio do torus

---
+++ Projeto/generator/include/shapes/torus.hpp
@@ -0,0 +1,10 @@
+#ifndef TORUS_HPP
+#define TORUS_HPP
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+void generateTorus(int minorRadius, int majorRadius, int stacks, int slices, char *outputFile);
+
+#endif
