c67e90306f86b34227e8625d94918ccf1260198f
RodrigoOFernandes <rodrigofernan132004@gmail.com>
2025-03-26 12:05:56 +0000

    quase quase la

--- Projeto/generator/src/shapes/patch.cpp
+++ Projeto/generator/src/shapes/patch.cpp
@@ -62,11 +62,20 @@ void generatePatch(char *patch_file, int tesselation, char *outputFile) {
         std::string line;
         std::getline(file >> std::ws, line);
         std::istringstream iss(line);
+        
+        std::vector<int> patchIndices;
         int index;
-        while (iss >> index) {
-            patches[i].push_back(index);
+        while (patchIndices.size() < 16 && iss >> index) {
+            patchIndices.push_back(index);
             if (iss.peek() == ',') iss.ignore();
         }
+        
+        if (patchIndices.size() != 16) {
+            std::cerr << "Error: Patch " << i << " does not have 16 indices." << std::endl;
+            continue;
+        }
+        
+        patches[i] = patchIndices;
     }
     
     int numControlPoints;
@@ -81,6 +90,10 @@ void generatePatch(char *patch_file, int tesselation, char *outputFile) {
     for (const auto& patch : patches) {
         std::vector<ControlPoint> patchPoints(16);
         for (int i = 0; i < 16; i++) {
+            if (patch[i] < 0 || patch[i] >= numControlPoints) {
+                std::cerr << "Invalid control point index: " << patch[i] << std::endl;
+                continue;
+            }
             patchPoints[i] = controlPoints[patch[i]];
         }
         
@@ -97,18 +110,22 @@ void generatePatch(char *patch_file, int tesselation, char *outputFile) {
             }
         }
         
-        // Generate triangles
-        for (int i = 0; i < tesselation; i++) {
-            for (int j = 0; j < tesselation; j++) {
-                // First triangle
-                outFile << surfacePoints[i][j].x << " " << surfacePoints[i][j].y << " " << surfacePoints[i][j].z << "\n";
-                outFile << surfacePoints[i+1][j+1].x << " " << surfacePoints[i+1][j+1].y << " " << surfacePoints[i+1][j+1].z << "\n";
-                outFile << surfacePoints[i+1][j].x << " " << surfacePoints[i+1][j].y << " " << surfacePoints[i+1][j].z << "\n";
+        // Generate triangles following right-hand rule (single triangle per square)
+        for (int i = 0; i < tesselation ; i++) {
+            for (int j = 0; j < tesselation ; j++) {
+                // Get four corner points of the current grid square
+                ControlPoint p1 = surfacePoints[i][j];      // Bottom-left
+                ControlPoint p2 = surfacePoints[i+1][j];    // Top-left
+                ControlPoint p3 = surfacePoints[i][j+1];    // Bottom-right
+                ControlPoint p4 = surfacePoints[i+1][j+1];  // Top-right
                 
-                // Second triangle
-                outFile << surfacePoints[i][j].x << " " << surfacePoints[i][j].y << " " << surfacePoints[i][j].z << "\n";
-                outFile << surfacePoints[i][j+1].x << " " << surfacePoints[i][j+1].y << " " << surfacePoints[i][j+1].z << "\n";
-                outFile << surfacePoints[i+1][j+1].x << " " << surfacePoints[i+1][j+1].y << " " << surfacePoints[i+1][j+1].z << "\n";
+                // First triangle: (p1, p2, p3)
+                outFile << p1.x << " " << p1.y << " " << p1.z << "\n";
+                outFile << p4.x << " " << p4.y << " " << p4.z << "\n";
+                 outFile << p2.x << " " << p2.y << " " << p2.z << "\n";
+                outFile << p1.x << " " << p1.y << " " << p1.z << "\n";
+                 outFile << p3.x << " " << p3.y << " " << p3.z << "\n";
+                outFile << p4.x << " " << p4.y << " " << p4.z << "\n"; 
             }
         }
     }
