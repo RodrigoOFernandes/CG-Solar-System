81b21b8b2d7e9e71855090d19c9fbc74a5bc5044
RodrigoOFernandes <rodrigofernan132004@gmail.com>
2025-03-26 11:44:46 +0000

    teapot: better

--- Projeto/generator/src/shapes/patch.cpp
+++ Projeto/generator/src/shapes/patch.cpp
@@ -9,23 +9,26 @@ struct ControlPoint {
     float x, y, z;
 };
 
-// Compute Bernstein polynomial
-float bernstein(int i, int n, float t) {
-    float result = 1.0f;
-    for (int j = 1; j <= i; ++j) {
-        result *= (n - j + 1.0f) / j;
+float bernstein(int i, float t) {
+    switch (i) {
+        case 0:
+            return std::pow(1 - t, 3);
+        case 1:
+            return 3 * t * std::pow(1 - t, 2);
+        case 2:
+            return 3 * std::pow(t, 2) * (1 - t);
+        case 3:
+            return std::pow(t, 3);
+        default:
+            return 0;
     }
-    return result * std::pow(t, i) * std::pow(1 - t, n - i);
 }
 
-// Compute a point on a Bézier surface using more efficient algorithm
 ControlPoint bezierPoint(const std::vector<ControlPoint>& patch, float u, float v) {
     ControlPoint p = {0, 0, 0};
     for (int i = 0; i < 4; i++) {
-        float bu = bernstein(i, 3, u);
         for (int j = 0; j < 4; j++) {
-            float bv = bernstein(j, 3, v);
-            float b = bu * bv;
+            float b = bernstein(i, u) * bernstein(j, v);
             
             p.x += patch[i * 4 + j].x * b;
             p.y += patch[i * 4 + j].y * b;
@@ -81,35 +84,31 @@ void generatePatch(char *patch_file, int tesselation, char *outputFile) {
             patchPoints[i] = controlPoints[patch[i]];
         }
         
-        // Improved tessellation approach with fewer triangles
-        std::vector<ControlPoint> surfacePoints((tesselation + 1) * (tesselation + 1));
+        // Create points for the current tessellation level
+        std::vector<std::vector<ControlPoint>> surfacePoints(tesselation + 1, 
+            std::vector<ControlPoint>(tesselation + 1));
         
         // Precompute surface points
         for (int i = 0; i <= tesselation; i++) {
             float u = (float)i / tesselation;
             for (int j = 0; j <= tesselation; j++) {
                 float v = (float)j / tesselation;
-                surfacePoints[i * (tesselation + 1) + j] = bezierPoint(patchPoints, u, v);
+                surfacePoints[i][j] = bezierPoint(patchPoints, u, v);
             }
         }
         
-        // Generate triangles more efficiently
+        // Generate triangles
         for (int i = 0; i < tesselation; i++) {
             for (int j = 0; j < tesselation; j++) {
-                int idx00 = i * (tesselation + 1) + j;
-                int idx10 = (i + 1) * (tesselation + 1) + j;
-                int idx01 = i * (tesselation + 1) + (j + 1);
-                int idx11 = (i + 1) * (tesselation + 1) + (j + 1);
-                
                 // First triangle
-                outFile << surfacePoints[idx10].x << " " << surfacePoints[idx10].y << " " << surfacePoints[idx10].z << "\n";
-                outFile << surfacePoints[idx01].x << " " << surfacePoints[idx01].y << " " << surfacePoints[idx01].z << "\n";
-                outFile << surfacePoints[idx00].x << " " << surfacePoints[idx00].y << " " << surfacePoints[idx00].z << "\n";
+                outFile << surfacePoints[i][j].x << " " << surfacePoints[i][j].y << " " << surfacePoints[i][j].z << "\n";
+                outFile << surfacePoints[i+1][j+1].x << " " << surfacePoints[i+1][j+1].y << " " << surfacePoints[i+1][j+1].z << "\n";
+                outFile << surfacePoints[i+1][j].x << " " << surfacePoints[i+1][j].y << " " << surfacePoints[i+1][j].z << "\n";
                 
                 // Second triangle
-                outFile << surfacePoints[idx11].x << " " << surfacePoints[idx11].y << " " << surfacePoints[idx11].z << "\n";
-                outFile << surfacePoints[idx01].x << " " << surfacePoints[idx01].y << " " << surfacePoints[idx01].z << "\n";
-                outFile << surfacePoints[idx10].x << " " << surfacePoints[idx10].y << " " << surfacePoints[idx10].z << "\n";
+                outFile << surfacePoints[i][j].x << " " << surfacePoints[i][j].y << " " << surfacePoints[i][j].z << "\n";
+                outFile << surfacePoints[i][j+1].x << " " << surfacePoints[i][j+1].y << " " << surfacePoints[i][j+1].z << "\n";
+                outFile << surfacePoints[i+1][j+1].x << " " << surfacePoints[i+1][j+1].y << " " << surfacePoints[i+1][j+1].z << "\n";
             }
         }
     }
