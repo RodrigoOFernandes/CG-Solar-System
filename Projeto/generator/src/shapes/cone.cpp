62fe2d4c04ea64824761c6874fe35ae9d900ea5e
RodrigoOFernandes <A104175@alunos.uminho.pt>
2025-05-08 20:42:02 +0100

    feat: fase 4 feita

--- Projeto/generator/src/shapes/cone.cpp
+++ Projeto/generator/src/shapes/cone.cpp
@@ -4,10 +4,19 @@
 #include <fstream>
 #include <cmath>
 
+void normalize(float& x, float& y, float& z) {
+    float length = sqrt(x * x + y * y + z * z);
+    if (length != 0.0f) {
+        x /= length;
+        y /= length;
+        z /= length;
+    }
+}
+
 void generateCone(int radius, int height, int slices, int stacks, char* outputFile) {
     std::string fullPath = "../models/" + std::string(outputFile);
     std::ofstream outFile(fullPath);
-  
+
     if (!outFile.is_open()) {
         std::cerr << "Error opening output file: " << outputFile << std::endl;
         return;
@@ -17,10 +26,11 @@ void generateCone(int radius, int height, int slices, int stacks, char* outputFi
 
     float sliceStep = (2.0f * M_PI) / slices;
     float stackStep = (float)height / stacks;
-    
+
+    // Base
     for (int i = 0; i < slices; i++) {
         float theta1 = i * sliceStep;
-        float theta2 = (i + 1) % slices * sliceStep;  
+        float theta2 = (i + 1) % slices * sliceStep;
 
         float x1 = radius * sin(theta1);
         float z1 = radius * cos(theta1);
@@ -28,42 +38,66 @@ void generateCone(int radius, int height, int slices, int stacks, char* outputFi
         float z2 = radius * cos(theta2);
         float y = 0.0f;
 
-        outFile << "0.0 0.0 0.0\n";  
-        outFile << x2 << " " << y << " " << z2 << std::endl;
-        outFile << x1 << " " << y << " " << z1 << std::endl;
+        // Centro
+        outFile << "0.0 0.0 0.0  0.0 -1.0 0.0  0.5 0.5\n";
+        // Borda 1
+        outFile << x2 << " " << y << " " << z2 << "  0.0 -1.0 0.0  "
+                << (0.5 + 0.5 * sin(theta2)) << " " << (0.5 + 0.5 * cos(theta2)) << "\n";
+        // Borda 2
+        outFile << x1 << " " << y << " " << z1 << "  0.0 -1.0 0.0  "
+                << (0.5 + 0.5 * sin(theta1)) << " " << (0.5 + 0.5 * cos(theta1)) << "\n";
     }
 
+    // Corpo lateral
     for (int i = 0; i < slices; i++) {
         float theta1 = i * sliceStep;
-        float theta2 = (i + 1) % slices * sliceStep; 
+        float theta2 = (i + 1) % slices * sliceStep;
 
         for (int j = 0; j < stacks; j++) {
             float y1 = j * stackStep;
             float y2 = (j + 1) * stackStep;
 
-            float r1 = radius * (1.0f - (float)j / stacks);     
-            float r2 = radius * (1.0f - (float)(j + 1) / stacks); 
+            float r1 = radius * (1.0f - (float)j / stacks);
+            float r2 = radius * (1.0f - (float)(j + 1) / stacks);
 
-            float x1_1 = r1 * sin(theta1);
-            float z1_1 = r1 * cos(theta1);
-            float x1_2 = r1 * sin(theta2);
-            float z1_2 = r1 * cos(theta2);
+            float x11 = r1 * sin(theta1);
+            float z11 = r1 * cos(theta1);
+            float x12 = r1 * sin(theta2);
+            float z12 = r1 * cos(theta2);
+            float x21 = r2 * sin(theta1);
+            float z21 = r2 * cos(theta1);
+            float x22 = r2 * sin(theta2);
+            float z22 = r2 * cos(theta2);
 
-            float x2_1 = r2 * sin(theta1);
-            float z2_1 = r2 * cos(theta1);
-            float x2_2 = r2 * sin(theta2);
-            float z2_2 = r2 * cos(theta2);
+            // Normais
+            float nx11 = x11, ny11 = radius / (float)height, nz11 = z11;
+            float nx12 = x12, ny12 = radius / (float)height, nz12 = z12;
+            float nx21 = x21, ny21 = radius / (float)height, nz21 = z21;
+            float nx22 = x22, ny22 = radius / (float)height, nz22 = z22;
 
-            outFile << x2_1 << " " << y2 << " " << z2_1 << "\n";
-            outFile << x1_1 << " " << y1 << " " << z1_1 << "\n";
-            outFile << x1_2 << " " << y1 << " " << z1_2 << "\n";
-            
-            outFile << x2_2 << " " << y2 << " " << z2_2 << "\n";
-            outFile << x2_1 << " " << y2 << " " << z2_1 << "\n";
-            outFile << x1_2 << " " << y1 << " " << z1_2 << "\n";
+            normalize(nx11, ny11, nz11);
+            normalize(nx12, ny12, nz12);
+            normalize(nx21, ny21, nz21);
+            normalize(nx22, ny22, nz22);
 
+            // Texture coords
+            float u1 = (float)i / slices;
+            float u2 = (float)(i + 1) / slices;
+            float v1 = (float)j / stacks;
+            float v2 = (float)(j + 1) / stacks;
+
+            // Triângulo 1
+            outFile << x21 << " " << y2 << " " << z21 << "  " << nx21 << " " << ny21 << " " << nz21 << "  " << u1 << " " << v2 << "\n";
+            outFile << x11 << " " << y1 << " " << z11 << "  " << nx11 << " " << ny11 << " " << nz11 << "  " << u1 << " " << v1 << "\n";
+            outFile << x12 << " " << y1 << " " << z12 << "  " << nx12 << " " << ny12 << " " << nz12 << "  " << u2 << " " << v1 << "\n";
+
+            // Triângulo 2
+            outFile << x22 << " " << y2 << " " << z22 << "  " << nx22 << " " << ny22 << " " << nz22 << "  " << u2 << " " << v2 << "\n";
+            outFile << x21 << " " << y2 << " " << z21 << "  " << nx21 << " " << ny21 << " " << nz21 << "  " << u1 << " " << v2 << "\n";
+            outFile << x12 << " " << y1 << " " << z12 << "  " << nx12 << " " << ny12 << " " << nz12 << "  " << u2 << " " << v1 << "\n";
         }
     }
 
     outFile.close();
 }
+
--- Projeto/generator/src/shapes/patch.cpp
+++ Projeto/generator/src/shapes/patch.cpp
@@ -10,19 +10,46 @@ struct ControlPoint {
     float x, y, z;
 };
 
+struct Vertex {
+    float x, y, z;
+    float nx, ny, nz;
+    float tx, ty;
+};
+
 int binomialCoefficient(int n, int i) {
     int res = 1;
     for(int k = 0; k < i; ++k) {
         res *= (n - k);
         res /= (k + 1);
     }
-
     return res;
 }
 
 float bernstein(int n, int i, float y) {
     int bin = binomialCoefficient(n, i);
     return bin * pow(1 - y, n - i) * pow(y, i);
+}
+
+void calculateNormal(ControlPoint p1, ControlPoint p2, ControlPoint p3, float& nx, float& ny, float& nz) {
+    float ux = p2.x - p1.x;
+    float uy = p2.y - p1.y;
+    float uz = p2.z - p1.z;
+    
+    float vx = p3.x - p1.x;
+    float vy = p3.y - p1.y;
+    float vz = p3.z - p1.z;
+    
+    nx = uy * vz - uz * vy;
+    ny = uz * vx - ux * vz;
+    nz = ux * vy - uy * vx;
+    
+    // Normalize
+    float length = sqrt(nx * nx + ny * ny + nz * nz);
+    if (length > 0) {
+        nx /= length;
+        ny /= length;
+        nz /= length;
+    }
 }
 
 void generatePatch(char* patches_file, int tesselation, char* output_file) {    
@@ -104,14 +131,49 @@ void generatePatch(char* patches_file, int tesselation, char* output_file) {
                     ControlPoint p01 = grid_points[index01];
                     ControlPoint p10 = grid_points[index10]; 
                     ControlPoint p11 = grid_points[index11];
-                  
-                    out << p00.x << " " << p00.y << " " << p00.z << "\n";
-                    out << p11.x << " " << p11.y << " " << p11.z << "\n";
-                    out << p10.x << " " << p10.y << " " << p10.z << "\n";
+                    
+                    // Calcular normais para cada triângulo
+                    float nx1, ny1, nz1; // Normal do primeiro triângulo
+                    float nx2, ny2, nz2; // Normal do segundo triângulo
+                    
+                    calculateNormal(p00, p11, p10, nx1, ny1, nz1);
+                    calculateNormal(p00, p01, p11, nx2, ny2, nz2);
+                    
+                    // Coordenadas de textura
+                    float u00 = (float)iu / tesselation;
+                    float v00 = (float)iv / tesselation;
+                    float u01 = (float)iu / tesselation;
+                    float v01 = (float)(iv + 1) / tesselation;
+                    float u10 = (float)(iu + 1) / tesselation;
+                    float v10 = (float)iv / tesselation;
+                    float u11 = (float)(iu + 1) / tesselation;
+                    float v11 = (float)(iv + 1) / tesselation;
+                    
+                    // Primeiro triângulo (p00, p11, p10)
+                    out << p00.x << " " << p00.y << " " << p00.z << " ";
+                    out << nx1 << " " << ny1 << " " << nz1 << " ";
+                    out << u00 << " " << v00 << "\n";
+                    
+                    out << p11.x << " " << p11.y << " " << p11.z << " ";
+                    out << nx1 << " " << ny1 << " " << nz1 << " ";
+                    out << u11 << " " << v11 << "\n";
+                    
+                    out << p10.x << " " << p10.y << " " << p10.z << " ";
+                    out << nx1 << " " << ny1 << " " << nz1 << " ";
+                    out << u10 << " " << v10 << "\n";
 
-                    out << p00.x << " " << p00.y << " " << p00.z << "\n";  
-                    out << p01.x << " " << p01.y << " " << p01.z << "\n";
-                    out << p11.x << " " << p11.y << " " << p11.z << "\n";
+                    // Segundo triângulo (p00, p01, p11)
+                    out << p00.x << " " << p00.y << " " << p00.z << " ";
+                    out << nx2 << " " << ny2 << " " << nz2 << " ";
+                    out << u00 << " " << v00 << "\n";
+                    
+                    out << p01.x << " " << p01.y << " " << p01.z << " ";
+                    out << nx2 << " " << ny2 << " " << nz2 << " ";
+                    out << u01 << " " << v01 << "\n";
+                    
+                    out << p11.x << " " << p11.y << " " << p11.z << " ";
+                    out << nx2 << " " << ny2 << " " << nz2 << " ";
+                    out << u11 << " " << v11 << "\n";
                 }
             }
         }
